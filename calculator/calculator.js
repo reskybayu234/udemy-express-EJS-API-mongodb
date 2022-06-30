cb42b68c3b736705d00216b8a4f21f69027ee8af
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-06-30 10:22:03 +0700

    post

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,13 +1,25 @@
 //jshint esversion:6
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+// app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.urlencoded({extended : true}));
+// app.use(express.json());
 const port = 3000;
 
 app.get("/", function(req,res){
   console.log(__dirname);
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req,res){
+  const requestBody = {
+    num1 : req.body.num1,
+    num2 : req.body.num2
+  }
+  console.log(requestBody)
+  res.send("Terimakasih " + requestBody.num1 + requestBody.num2);
 });
 
 app.listen(port,function(){
